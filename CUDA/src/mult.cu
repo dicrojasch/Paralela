abc70f91b164d37fb78da54a36e63d60b05e4e09
diego <dicrojasch>
2016-11-12 16:20:39 -0500

    Multiply matrix in CUDA works well, some tests has passed

--- CUDA/src/mult.cu
+++ CUDA/src/mult.cu
@@ -6,35 +6,27 @@
 #include <float.h>
 
 
-__global__ void matrixMult(const double *A, const double *B, double *C, int sizeMatrix){
-	int i, j;
-	int totalThreads = gridDim.x * blockDim.x;
-	int id = blockDim.x * blockIdx.x  + threadIdx.x;
-	int assign = sizeMatrix / totalThreads;
-	assign = assign < 0 ? 1:assign;
-	int start = id * assign;
-	int firstRow = 0, firstCol = 0;
-	for( i = start; i < assign; i++ ){
-		C[i] = 0;
-		firstRow = (i+1)/ sizeMatrix;
-		firstCol = i % sizeMatrix;
-		for( j = 0; j < sizeMatrix; j++ )
-			C[i] += A[firstRow+j] * B[(j*sizeMatrix)+firstCol];
-		if( i > (assign-1) &&  ((assign*totalThreads + id) < sizeMatrix)){
-			i = assign*totalThreads + id;
-			assign = i + 1;
+__global__ void matrixMult(const double *A, const double *B, double *C, int N){
+	int i, j, firstRow, firstCol, assign, start, totalThreads, id;
+	totalThreads = gridDim.x * blockDim.x;
+	id = blockDim.x * blockIdx.x  + threadIdx.x;
+	if( id < N*N ){
+		assign = (N*N) / totalThreads; assign = assign < 1 ? 1:assign;
+		start = id * assign;
+		for( i = start; i < start+assign; i++ ){
+			C[i] = 0; firstRow = (i / N)*N; firstCol = i % N;
+			for( j = 0; j < N; j++ )
+				C[i] += A[firstRow+j] * B[(j*N)+firstCol];
 		}
 	}
 }
 
 int main(int argc, char *argv[]){
+	int i, N = atoi(argv[1]), threadsPerBlock = atoi(argv[2]), blocksPerGrid = atoi(argv[3]);
+	size_t size = sizeof(double) * (N*N);
 	cudaError_t err = cudaSuccess;
-	int threadsPerBlock = atoi(argv[1]), N = atoi(argv[2]), blocksPerGrid = atoi(argv[3]);
-	int i, size;
 	srand(time(NULL));
-
-	size = sizeof(double) * (N*N);
-
+	threadsPerBlock = threadsPerBlock*blocksPerGrid > N*N ? (N*N/blocksPerGrid)+1 : threadsPerBlock;
 
 	double *h_A = (double *)malloc(size);
 	double *h_B = (double *)malloc(size);
@@ -45,46 +37,46 @@ int main(int argc, char *argv[]){
 	}
 
 	for( i = 0; i < N*N; i++){
-		h_A[i] = rand()/(float)RAND_MAX;
-		h_B[i] = rand()/(float)RAND_MAX;
+        h_A[i] = rand()/(double)RAND_MAX;
+        h_B[i] = rand()/(double)RAND_MAX;
 	}
 
-	double *d_A = (double *)malloc(size);
+	double *d_A = NULL;
 	err = cudaMalloc((void **)&d_A, size);
     if (err != cudaSuccess){
         fprintf(stderr, "Failed to allocate device matrix A (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
 
-	double *d_B = (double *)malloc(size);
+	double *d_B = NULL;
 	err = cudaMalloc((void **)&d_B, size);
     if (err != cudaSuccess){
         fprintf(stderr, "Failed to allocate device matrix B (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
 
-	double *d_C = (double *)malloc(size);
+	double *d_C = NULL;
 	err = cudaMalloc((void **)&d_C, size);
     if (err != cudaSuccess){
         fprintf(stderr, "Failed to allocate device matrix C (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
 
-    err = cudaMemcpy(h_A, d_A, size, cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
     if (err != cudaSuccess){
         fprintf(stderr, "Failed to copy Matrix A from host to device (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
 
-    err = cudaMemcpy(h_B, d_B, size, cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
     if (err != cudaSuccess){
         fprintf(stderr, "Failed to copy Matrix B from host to device (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
 
-    matrixMult<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N*N);
-    err = cudaGetLastError();
+    matrixMult<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N);
 
+    err = cudaGetLastError();
     if (err != cudaSuccess){
         fprintf(stderr, "Failed to launch kernel (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
@@ -96,12 +88,11 @@ int main(int argc, char *argv[]){
         exit(EXIT_FAILURE);
     }
 
-    for (int i = 0; i < N; i++){
-    	printf("%d ", h_C[i]);
-    	if( i%N == 0){
-    		printf("\n");
-    	}
-    }
+//    for (int i = 0; i < N*N; i++){
+//    	if( i%N == 0)
+//    	    printf("\n");
+//    	printf("%f ", h_C[i]);
+//    }
 
     err = cudaFree(d_A);
 	if (err != cudaSuccess){
